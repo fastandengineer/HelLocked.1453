942261d2d649ef0e2a721ddeafa56781b4dced2d
fedral01 <nazif9929@gmail.com>
2018-11-29 02:07:17 +0300

    Update CryptCreator.py

--- CryptCreator.py
+++ CryptCreator.py
@@ -5,12 +5,13 @@ import hashlib
 from getpass import getpass
 from hashlib import sha1
 import hashlib as hasher
+import sys
 
 print("\033[91m"+"""
 +--------------------------CryptCreator--------------------------+
 |								 |
 |                     Welcome to CryptCreator			 |
-|			       v1.0				 |
+|			       v1.1				 |
 |			 					 |
 | Create The Your MD5 - SHA1 - SHA224 - SHA256 - SHA384 - SHA512 |
 +----------------------------------------------------------------+
@@ -22,7 +23,7 @@ C0D3R T34M = HelLocked.1453
 while 1:
 
   #MD5
-  key = raw_input( "Enter the anything : " )
+  key = raw_input( "If you wanna exit, press CTRL + C\nEnter the anything : " )
   print("\033[93m"+"Your MD5 = " + hashlib.md5( key ).hexdigest()+"\033[0m")
 
   #SHA1
